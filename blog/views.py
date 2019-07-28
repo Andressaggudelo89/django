ccf99a460f3bca99554af767505dd1b15e068dfa
Andressaggudelo <cagudeloa@uninorte.edu.co>
2019-07-28 14:59:11 -0500

    User registration working already (via '/register/', not the bottom). No login yet.

--- blog/views.py
+++ blog/views.py
@@ -1,33 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
 # Create your views here.
-
-posts = [
-	{
-		'author' : 'Andres Agg',
-		'title' : 'Blog post 1',
-		'content' : 'First post Content',
-		'date_posted' : 'July 27, 2016'
-	},
-	{
-		'author' : 'Daniela Nav',
-		'title' : 'Blog post 2',
-		'content' : 'Second post Content',
-		'date_posted' : 'July 27, 2017'
-	},
-	{
-		'author' : 'Leyanis Tsk',
-		'title' : 'Blog post 3',
-		'content' : 'Third post Content',
-		'date_posted' : 'May 31, 2018'
-	}
-]
 
 
 def home(request):
 	context = {
-		'posts': posts
+		'posts': Post.objects.all()
 	}
 	return render(request, 'blog/home.html', context)
 
