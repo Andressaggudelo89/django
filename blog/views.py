11e73064b49134751e17005b912ca84f740da15d
Andressaggudelo <cagudeloa@uninorte.edu.co>
2019-07-30 23:20:41 -0500

    Auth, Registration, Login and Logout working.

--- blog/views.py
+++ blog/views.py
@@ -6,10 +6,10 @@ from .models import Post
 
 
 def home(request):
-	context = {
+	'''context = {
 		'posts': Post.objects.all()
-	}
-	return render(request, 'blog/home.html', context)
+	}'''
+	return render(request, 'blog/home.html')
 
 def about(request):
 	return render(request, 'blog/about.html', {'title': 'About'})
--- mytwitt/settings.py
+++ mytwitt/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +123,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+LOGIN_REDIRECT_URL = 'blog-home'
+LOGIN_URL='login'
